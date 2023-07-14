76241b95b4ccc8dd4d2b5d20601a1b7e2627e4d2
Umberto Toppino <umberto.toppino@hotmail.com>
2023-07-14 17:05:25 +0200

    Fix temperature mismatch

--- Scripts/Test_simulation.m
+++ Scripts/Test_simulation.m
@@ -7,12 +7,12 @@ t = t';
 % Generate data signals
 current         = [t,ones(size(t))*5];        % [A]
 init_cap        = [t,ones(size(t))*50];       % [Ah]
-storage_time    = [t,ones(size(t))*10];       % ?
-temperature     = [t,ones(size(t))*293];      % [°K]
-cycle_n         = [t,ones(size(t))*1];        % [n]
+storage_time    = [t,ones(size(t))*12];       % [months]
+temperature     = [t,ones(size(t))*25];       % [°C]
+cycle_n         = [t,ones(size(t))*600];        % [n]
 
 % Control signals
-v_min           = [t,ones(size(t))*3.2];
+v_min           = [t,ones(size(t))*2.5];
 
 % Launch Simulink simulation
 modelName       = 'test_simulation';
@@ -28,3 +28,20 @@ out_ccf     = baseline.ccf;
 plot(out_v_batt)
 hold on;
 plot(out_ccf)
+hold on;
+
+% Change input parameters
+%current         = [t,ones(size(t))*10];        % [A]
+%cycle_n         = [t,ones(size(t))*600];        % [n]
+temperature     = [t,ones(size(t))*50];       % [°C]
+
+% Launch second simulation
+baseline        = sim(siminBaseline);
+
+%Log output
+out_v_batt  = baseline.v_batt;
+out_ccf     = baseline.ccf;
+
+% Plot results
+plot(out_v_batt)
+hold on;
